1c82b322d6251534b9c487626c5dc5d0f4c7a161
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-17 16:34:15 +0100

    weather

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-let forecast = document.getElementById("forecast");
-let weatherDay = document.getElementById("weather");
+let temperature = document.getElementById("temperature");
+let nameCity = document.getElementById("nameCity");
+let description = document.getElementById("description");
+let humidity = document.getElementById("humidity");
+let tempMin = document.getElementById("temp-min");
+let tempMax = document.getElementById("temp-max");
+let sunrise = document.getElementById("sunrise");
+let sunset = document.getElementById("sunset");
+let image = document.getElementById("image");
+let container = document.getElementById("container");
+let date = document.getElementById("date");
 const valid = document.getElementById("valid");
+
+container.style.height = innerHeight + "px";
+
+setInterval(()=>date.innerHTML = new Date().toLocaleTimeString(),1000);//date
 
 valid.addEventListener("click", () => {
     let city = document.getElementById("city").value;
@@ -11,27 +24,42 @@ valid.addEventListener("click", () => {
     xhr.open("GET", requestURL);
     xhr.responseType = "json";
 
+    /**
+     * server response
+     */
     xhr.onload = function () {
         if(xhr.status !== 200) {
             return;
         }
         let response = xhr.response;
 
-        weatherDay.innerHTML += "<br>" + city + "<br>";
-        weatherDay.innerHTML += "Température: " + Math.ceil(response.main.temp) + "°C" + "<br>";
-        weatherDay.innerHTML += response.weather[0].description + "<br>";
-        weatherDay.innerHTML += "Humidité: " + Math.ceil(response.main.humidity) + "%" + "<br>";
-        weatherDay.innerHTML += "Température min : " + Math.ceil(response.main.temp_min) + "°C" + "<br>";
-        weatherDay.innerHTML += "Temperature max : " + Math.ceil(response.main.temp_max) + "°C" + "<br>";
+        switch (response.weather[0].description) {
+            case "ciel dégagé" :
+                image.style.backgroundImage = "url('../img/cielDegage.png')";
+                break;
+            case "couvert" :
+                image.style.backgroundImage = "url('../img/couvert.png')";
+                break;
+            case "légères chutes de neige" :
+                image.style.backgroundImage = "url('../img/chutesDeNeige.png')";
+                break;
+            case "légère pluie" :
+                image.style.backgroundImage = "url('../img/légèrePluie.png')";
+                break;
+
+        }
+        nameCity.innerHTML = "<br>" + city + "<br>";
+        temperature.innerHTML = Math.ceil(response.main.temp) + "°C" + "<br>";
+        description.innerHTML = response.weather[0].description + "<br>";
+        humidity.innerHTML = "Humidité: " + Math.ceil(response.main.humidity) + "%" + "<br>";
+        tempMin.innerHTML = "Température min : " + Math.ceil(response.main.temp_min) + "°C" + "<br>";
+        tempMax.innerHTML = "Temperature max : " + Math.ceil(response.main.temp_max) + "°C" + "<br>";
 
         let date = new Date(response.sys.sunrise * 1000).toLocaleTimeString();
-        weatherDay.innerHTML += "Lever du soleil : " + date +  "<br>";
+        sunrise.innerHTML = "Lever du soleil : " + date +  "<br>";
 
         date = new Date(response.sys.sunset * 1000).toLocaleTimeString();
-        weatherDay.innerHTML += "Coucher du soleil : " + date ;
-
-        forecast.innerHTML += response.forecast.time.from + date ;
-        forecast.innerHTML += response.forecast.time.to + (date * 4);
+        sunset.innerHTML = "Coucher du soleil : " + date ;
     }
     xhr.send();
 })
